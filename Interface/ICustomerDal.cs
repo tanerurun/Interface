a95d4ea1d956d3a08ac821832f0f8393509be36c
taner <taner@DESKTOP-8FBSMG9>
2021-09-04 18:18:46 +0300

    Add project files.

---
+++ Interface/ICustomerDal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    //Dal>data access layer
+  public  interface ICustomerDal
+    {
+        void add();
+        void update();
+        void delete();
+        //3 adet oparasyon
+    }
+    class SqlCustomerDal : ICustomerDal
+    {
+        public void add()
+        {
+            Console.WriteLine("Sql added");
+        }
+
+        public void delete()
+        {
+            Console.WriteLine("sql delete");
+        }
+
+        public void update()
+        {
+            Console.WriteLine("sql update");
+        }
+    }
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void add()
+        {
+            Console.WriteLine("oracle add");
+        }
+
+        public void delete()
+        {
+            Console.WriteLine("oracle delete");
+        }
+
+        public void update()
+        {
+            Console.WriteLine("Oracle update");
+        }
+    }
+
+  public  class customerManager
+    {
+        public void add(ICustomerDal customerDal)
+        {
+            customerDal.add();
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Interface;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerDal[] customerDal = new ICustomerDal[2]
+            {
+                new SqlCustomerDal(),
+                new OracleCustomerDal()
+            };
+            customerManager customerManager =new customerManager();
+            customerManager.add(new SqlCustomerDal());
+             
+            PersonManager personManager = new PersonManager();
+            personManager.add(new Customer { id = 1, firstName = "Taner", lastName = "ürün" });
+
+
+            Customer customer = new Customer();
+            customer.firstName = "Haşim";
+            personManager.add(customer);
+
+           
+          
+
+            
+
+            Console.ReadLine();
+
+        }
+    }
+
+
+    interface IPerson //soyut
+    {
+        int id { get; set; }
+        string firstName { get; set; }
+        string lastName { get; set; }
+    }
+    class Customer : IPerson
+    //somut Customer
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+    //somut student
+    class Student : IPerson
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void add(Customer customer)
+        {
+            Console.WriteLine(customer.firstName);
+        }
+
+    }
+}
